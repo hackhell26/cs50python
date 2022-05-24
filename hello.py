d094cac4d47c54fc78e58a973aafcc818a8dc947
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-24 13:08:49 +0000

    printing with two arguments

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 # Ask user for their name
 name = input("What is your name? ")
 
-# Printing in the same line modifying the default end argument behavior
-print("hello, ", end="")
-print(name)
+# Printing in the same line
+print("hello,", name)
