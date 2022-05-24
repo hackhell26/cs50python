3d780d3141b9d24cf2115cb99e130a1162a5ebc7
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-24 17:06:20 +0000

    rounded division with 2 decimal

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 x = float(input("What's x: "))
 y = float(input("What's y: "))
 
-z = x / y
+# Passing two arguments (x/y)&(2) to the function to round the division with 2 decimals
+z = round(x / y, 2)
 
 print(z)
