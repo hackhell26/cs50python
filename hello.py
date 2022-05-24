b11f96071c0ef7f3f51f05a8ce65a6b9ad629696
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-24 13:44:24 +0000

    title based capitalization

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ name = input("What is your name? ")
 # Remove white space from a str
 name = name.strip()
 
-# Capitalize user's name
-name = name.capitalize()
+# Title based capitalization
+name = name.title()
 
 # Say hello to user
 print(f"hello, {name}")
