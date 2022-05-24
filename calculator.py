62787a2c5b5e4953fc8973349ba6e9cd5eb6d3f6
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-24 17:09:42 +0000

    rounded without round funct

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 x = float(input("What's x: "))
 y = float(input("What's y: "))
 
-# Passing two arguments (x/y)&(2) to the function to round the division with 2 decimals
-z = round(x / y, 2)
+z = x / y
 
-print(z)
+# Using "f string" to say how many digits to print
+print(f"{z:.2f}")
