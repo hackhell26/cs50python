1317b424770ad5e68b874342b7760e0cf1ce115e
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-24 13:53:40 +0000

    strip().title() in the same line

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
 # Ask user for their name
 name = input("What is your name? ")
 
-# Remove white space from a str
-name = name.strip()
-
-# Title based capitalization
-name = name.title()
+# Remove white space from a str and capitalized user's name
+name = name.strip().title()
 
 # Say hello to user
 print(f"hello, {name}")
