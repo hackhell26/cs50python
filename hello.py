e281642ba4a2fd9abdd66d9219a463df5293bf4b
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-24 17:27:12 +0000

    adding parameter world

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-# Adding parameters to the function
-def hello(to):
+# If user does not type any input, it will print just "hello, world"
+def hello(to="world"):
     print("hello,", to)
 
+hello()
 name = input("What is your name? ")
 hello(name)
 
