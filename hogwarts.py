51bf1895ce5171d8475825fa6b48284808236d3a
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-26 19:33:28 +0000

    printing whole dictionary

--- hogwarts.py
+++ hogwarts.py
@@ -5,8 +5,5 @@ students = {
     "Draco": "Slyderin",
 }
 
-""" When you use a for loop in python, to iterate over a dictionary
-by desing it iterates over all of the keys"""
-
 for student in students:
-    print(student)
+    print(student, students[student])
