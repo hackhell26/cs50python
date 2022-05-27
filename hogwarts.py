14bc2311353be3100718708a8c9c985883ef8836
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-27 16:53:14 +0000

    Fri, May 27, 2022, 4:53 PM +00:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,10 +1,10 @@
 # A list of dictionaries
 students = [
-    {"name": "Hermione", "house": "Gryffindor", "patronus:": "Otther"},
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otther"},
     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
     {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
     {"name": "Draco", "house": "Slytherine", "patronus": None}
 ]
 
 for student in students:
-    print(student["name"], student["house"], sep=", ")
+    print(student["name"], student["house"], student["patronus"], sep=", ")
