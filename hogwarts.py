895215f4a84376f638bcae025b20f4edd55d35ae
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-27 16:50:34 +0000

    Fri, May 27, 2022, 4:50 PM +00:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,6 +1,6 @@
 # A list of dictionaries
 students = [
-    {"name": "Hermione", "house:": "Gryffindor", "patronus:": "Otther"},
+    {"name": "Hermione", "house": "Gryffindor", "patronus:": "Otther"},
     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
     {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
     {"name": "Draco", "house": "Slytherine", "patronus": None}
