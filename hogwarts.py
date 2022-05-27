db78c35682bf46a6fad36b3fafcd494797b48b04
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-27 16:44:05 +0000

    Fri, May 27, 2022, 4:44 PM +00:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,10 @@
 # One dictionary per student
 students = [
     {"name": "Hermione", "house:": "Gryffindor", "patronus:": "Otther"},
-    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"}
-    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"}
-
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Draco", "house": "Slytherine", "patronus": None}
 ]
+
 for student in students:
     print(student, students[student], sep=', ')
