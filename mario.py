27886422b5ca08298e62af0570e85b6819fb81ad
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-05-28 09:08:07 +0000

    square size 3

--- mario.py
+++ mario.py
@@ -1,8 +1,20 @@
 def main():
-    print_row(4)
+    print_square(3)
 
 
-def print_row(width):
-    print("?" * width)
+def print_square(size):
+
+    # For each row in the square
+    for i in range(size):
+
+        # For each brick in row
+        for j in range(size):
+
+            # Print brick
+            print("#", end='')
+
+        print()
+
+
 
 main()
