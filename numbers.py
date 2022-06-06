916b36296614bbfd870549bef6b484f605c94ce3
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-06-06 13:10:56 +0000

    defining get_int() function

--- numbers.py
+++ numbers.py
@@ -1,11 +1,17 @@
-# Re prompt to the user if it's input it's different to a integer
-# And avoid to exit the program
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not a integer")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? "))
+        except ValueError:
+            print("x is not a integer")
+        else:
+            break
+    return x
+
+
+main()
