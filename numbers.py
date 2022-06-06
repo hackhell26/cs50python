bb3cbc9bbeb874d1feb77179664fafdce67871ec
hackhell26 <95175755+hackhell26@users.noreply.github.com>
2022-06-06 12:57:05 +0000

    while loop to re prompt

--- numbers.py
+++ numbers.py
@@ -1,7 +1,11 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not a integer")
-# else statement to handle the NameError: "name x is not defined"
-else:
-    print(f"x is {x}")
+# Re prompt to the user if it's input it's different to a integer
+# And avoid to exit the program
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not a integer")
+    else:
+        break
+
+print(f"x is {x}")
